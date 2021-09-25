70be2e3104da52aac90c2ffc90aa4739a4213a2b
bx2nero <aa247@protonmail.com>
2021-09-25 21:46:05 +0530

    RMX2001: Aospify
    Change-Id: Ie9b843661478a676b4c340238531363817a92219
    Signed-off-by: bx2nero <aa247@protonmail.com>

--- aosp_RMX2001.mk
+++ aosp_RMX2001.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2001/device.mk)
 
-# Inherit some common Havoc OS stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_RMX2001
+PRODUCT_NAME := aosp_RMX2001
 PRODUCT_DEVICE := RMX2001
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G90T Series
