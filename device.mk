6092f0e889308fdd67c92f21ae3970b5c044dc50
Sukrut4778 <sukrut4778@gmail.com>
2021-10-02 22:52:27 +0530

    RMX2001: fingerprint/interfaces: Oppo -> Oplus * This is required since RUI 2.0
    Change-Id: I2b9f4bdc03ec8e5f6edd2e4060d0d8a619f0601e
    Signed-off-by: Sukrut4778 <sukrut4778@gmail.com>
    Signed-off-by: bx2nero <aa247@protonmail.com>

--- device.mk
+++ device.mk
@@ -68,9 +68,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.RMX2001 \
-    vendor.oppo.hardware.biometrics.fingerprint@2.1
-
+    android.hardware.biometrics.fingerprint@2.1-service.RMX2001
+     
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 
