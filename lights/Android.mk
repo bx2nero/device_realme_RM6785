5c10b0ae693b5dc28ae82f6cc3bd31e1d5bb5c6c
SamarV-121 <samarvispute121@gmail.com>
2021-11-20 12:37:45 +0530

    RMX2001: lights: Override stock lights AIDL
    Change-Id: I58a595c8f206e7b31d0f64889bb766b67970a160
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: bx2nero <aa247@protonmail.com>

---
+++ lights/Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.light@2.0-service.RMX2001
+LOCAL_MODULE_STEM := android.hardware.lights-service.mediatek
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_VNDK_VERSIONS)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS  := optional
+LOCAL_REQUIRED_MODULES := lights-mtk-default.xml
+
+LOCAL_SRC_FILES := \
+    service.cpp \
+    Light.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libbase \
+    libutils \
+    libhidlbase \
+    libhardware \
+    android.hardware.light@2.0
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lights-mtk-default.xml
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/vintf/manifest
+include $(BUILD_PREBUILT)
